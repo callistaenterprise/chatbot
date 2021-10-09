9ad369783d0c9be0fe336138af6c32dbc51be5c7
David Ström <david.strom@callistaenterprise.se>
2021-10-09 15:44:25 +0200

    Dedicated branch for GloVe

--- word2vec/d_glove/glove.py
+++ word2vec/d_glove/glove.py
@@ -1,19 +1,16 @@
 from scipy.linalg import svd
-from glove import Glove
-import numpy as np
 from datetime import datetime
 
 
 class GloVe(object):
 
-    def __init__(self, training_file, vocalbulary_size, vector_size):
-        self.model = Glove()
+    #def __init__(self, training_file, vocalbulary_size, vector_size):
+        # self.model = Glove()
         # self.model = glove.Glove(cooccurrance_matrix, vocab_size=vocalbulary_size, d=vector_size, alpha=0.75, x_max=100.0)
-        # U, sigma, Vt = svd(X)
 
-
-    def train_model(self, epochs):
+    def train_model(self, epochs, X):
         batch_size = 32
+        U, sigma, Vt = svd(X)
         for epoch in range(epochs):
             loss = 0.
             epoch_start = datetime.now()
