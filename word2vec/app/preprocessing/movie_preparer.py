965285fa062188127bfd8e6b6db89004862b69a3
David Ström <david.strom@callistaenterprise.se>
2021-10-16 11:19:17 +0200

    refactoring

---
+++ word2vec/app/preprocessing/movie_preparer.py
@@ -0,0 +1,48 @@
+from os import path
+
+
+class MoviePreparer(object):
+    delimiter = ' +++$+++ '
+    new_line = '\r\n'
+
+    def __init__(self, movie_lines, movie_conversations):
+        self.conversation_lines = []
+        self.lines = dict()
+        self.conversations = []
+        self.movie_scripts = movie_lines
+        dir_name = path.dirname(__file__)
+        self.conversation_file = path.join(dir_name, movie_conversations)
+
+    def parse_movie_files(self):
+        dir_name = path.dirname(__file__)
+        prepared_data_file = path.join(dir_name, '../../data/preprocessing/prepared_movie_data.txt')
+        if not path.exists(prepared_data_file):
+            with open(self.conversation_file) as mc:
+                for line in mc:
+                    line = line.rstrip(MoviePreparer.new_line)
+                    line = line.split(MoviePreparer.delimiter)[3]
+                    self.conversation_lines.append(line)
+
+            with open(self.movie_scripts, encoding='utf-8', errors='ignore') as ml:
+                for line in ml:
+                    line = line.rstrip(MoviePreparer.new_line)
+                    line = line.split(MoviePreparer.delimiter)
+                    if len(line) == 5:
+                        key = line[0]
+                        value = line[4]
+                        self.lines[key] = value
+
+            for conversation_line in self.conversation_lines:
+                conversation_list = eval(conversation_line)
+                conversation = []
+                for line_ref in conversation_list:
+                    if line_ref in self.lines:
+                        conversation.append(self.lines[line_ref])
+                self.conversations.append(" ".join(conversation))
+
+            with open(prepared_data_file, "w") as prepared_data:
+                for line in self.conversations:
+                    prepared_data.write(line)
+                    prepared_data.write('\n')
+
+        return prepared_data_file
--- word2vec/tests/blog_preparer_test.py
+++ word2vec/tests/blog_preparer_test.py
@@ -1,5 +1,2 @@
-import pytest
-from preprocessing import blog_preparer
-
 def test_clean_blog():
     print('Hello')
