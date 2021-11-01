e04c52db3d68319308f0a320e5e3de111a01bcf9
David Ström <david.strom@callistaenterprise.se>
2021-11-01 08:36:28 +0100

    Refactored training_sample_generator and added unit test

--- word2vec/app/preprocessing/training_data_builder.py
+++ word2vec/app/preprocessing/training_data_builder.py
@@ -15,10 +15,6 @@ class TrainingDataBuilder(object):
         self.source_file = cleaned_file
         self.vocabulary = set()
         self.dry_run = dry_run
-        # self.word2id_file = path.join(dir_name, '../../data/training_data/word2id.dat')
-        # self.word2id = self.__load_word_2_id() if path.exists(self.word2id_file) else dict()
-        # self.id2word_file = path.join(dir_name, '../../data/training_data/id2word.dat')
-        # self.id2word = self.__load_id_2_word() if path.exists(self.id2word_file) else dict()
         self.window_size = window_size
         self.cbow_training_data_file = path.join(
             dir_name, "../../data/training_data/cbow_training_data.dat"
@@ -29,40 +25,13 @@ class TrainingDataBuilder(object):
         self.glove_training_data_file = path.join(
             dir_name, "../../data/training_data/glove_training_data.dat"
         )
-        self.tokenizer = Tokenizer()
+        self.tokenizer = Tokenizer(oov_token='UNK')
 
     def __test_generator(self):
         with open(self.source_file) as training_data:
             for line in training_data:
                 yield line
 
-    # def __parse_training_data(self):
-    # 	self.tokenizer.fit_on_texts(self.__test_generator())
-
-    # def __update_vocabulary(self, line: str):
-    # 	words = line.split(' ')
-    # 	self.vocabulary.update(words)
-    #
-    # def __update_vocabulary_with_count(self, line: str, word_counter: dict):
-    # 	words = line.split(' ')
-    # 	self.vocabulary.update(words)
-    # 	for word in words:
-    # 		if word in word_counter.keys():
-    # 			word_counter[word] = word_counter[word] + 1
-    # 		else:
-    # 			word_counter[word] = 1
-    # 	return word_counter
-    #
-    # def __update_dictionaries(self, line, index):
-    # 	# When we cannot create a full sample from our training sentences
-    # 	# we will pad them so all train samples have same size
-    # 	for word in line.split(' '):
-    # 		if word not in self.word2id:
-    # 			self.word2id[word] = index
-    # 			self.id2word[index] = word
-    # 			index += 1
-    # 	return index
-    #
     def line_to_word_ids(self, line):
         return self.tokenizer.texts_to_sequences(line)
 
@@ -119,43 +88,31 @@ class TrainingDataBuilder(object):
         return len(self.tokenizer.word_index) + 1, X_y
 
     def build_sg_training_data(self):
-        X_y = dict()
-        if path.exists(self.sg_training_data_file) and path.exists(self.word2id_file):
+        self.tokenizer.fit_on_texts(self.__test_generator())
+        vocabulary_size = len(self.tokenizer.word_index) + 1
+        if path.exists(self.sg_training_data_file) and self.dry_run is False:
             X_y = self.__load_training_data(self.sg_training_data_file)
-            # self.word2id = self.__load_word_2_id()
-            vocabulary_size = len(self.word2id)
         else:
-            X = []
-            self.word2id["PAD"] = 0
-            current_dictionary_index = 1
-            with open(self.source_file) as training_data:
-                for line in training_data:
-                    line = line.rstrip(TrainingDataBuilder.new_line)
-                    self.__update_vocabulary(line)
-                    current_dictionary_index = self.__update_dictionaries(
-                        line, current_dictionary_index
-                    )
-                    word_ids = self.line_to_word_ids(line)
-                    X.append(word_ids)
-
-            self.__save_word_2_id()
-            self.__save_id_2_word()
-            vocabulary_size = len(self.word2id)
-            sampling_table = sequence.make_sampling_table(vocabulary_size)
+            X_y = dict()
             training_samples_x = []
             training_samples_y = []
-            for word_ids in X:
-                word_pairs, labels = sequence.skipgrams(
-                    word_ids,
-                    vocabulary_size,
-                    window_size=self.window_size,
-                    sampling_table=sampling_table,
-                )
-                training_samples_x.extend(word_pairs)
-                training_samples_y.extend(labels)
+            sampling_table = sequence.make_sampling_table(vocabulary_size)
+            with open(self.source_file) as training_data:
+                for line in training_data:
+                    word_ids = self.tokenizer.texts_to_sequences([line])[0]
+                    word_pairs, labels = sequence.skipgrams(
+                        word_ids,
+                        vocabulary_size,
+                        window_size=self.window_size,
+                        sampling_table=sampling_table,
+                    )
+                    training_samples_x.extend(word_pairs)
+                    training_samples_y.extend(labels)
+
             X_y["X"] = training_samples_x
             X_y["y"] = training_samples_y
-            self.__save_training_data(self.sg_training_data_file, X_y)
+            if not self.dry_run:
+                self.__save_training_data(self.sg_training_data_file, X_y)
         return vocabulary_size, X_y
 
     def build_glove_training_data(self):
@@ -185,23 +142,6 @@ class TrainingDataBuilder(object):
         print("Most rare words: {}".format(list(sorted_word_count)[:100]))
         vocabulary_size = len(self.word2id)
         return vocabulary_size, cooccurrance
-
-    # def __save_id_2_word(self):
-    # 	with open(self.id2word_file, 'wb') as f:
-    # 		pickle.dump(self.id2word, f)
-    # 	return self.id2word_file
-    #
-    # def __load_id_2_word(self):
-    # 	with open(self.id2word_file, 'rb') as f:
-    # 		return pickle.load(f)
-    #
-    # def __save_word_2_id(self):
-    # 	with open(self.word2id_file, 'wb') as f:
-    # 		pickle.dump(self.word2id, f)
-    #
-    # def __load_word_2_id(self):
-    # 	with open(self.word2id_file, 'rb') as f:
-    # 		return pickle.load(f)
 
     def __save_training_data(self, training_data_file, X_y):
         with open(training_data_file, "wb") as f:
--- word2vec/tests/training_data_builder_test.py
+++ word2vec/tests/training_data_builder_test.py
@@ -5,11 +5,18 @@ from os import path
 
 class TrainingDataBuilderTest(unittest.TestCase):
 
-    def test_generate_training_samples(self):
+    def test_generate_cbow_training_samples(self):
         dir_name = path.dirname(__file__)
         self.training_data_builder = TrainingDataBuilder(cleaned_file=path.join(dir_name, 'test_cleaned_train_data.txt'), window_size=2, dry_run=True)
         vocabulary_size, X_y = self.training_data_builder.build_cbow_training_data()
-        self.assertEqual(vocabulary_size, 10)
+        self.assertEqual(vocabulary_size, 11)
+
+    def test_generate_sg_training_samples(self):
+        dir_name = path.dirname(__file__)
+        self.training_data_builder = TrainingDataBuilder(cleaned_file=path.join(dir_name, 'test_cleaned_train_data.txt'), window_size=2, dry_run=True)
+        vocabulary_size, X_y = self.training_data_builder.build_sg_training_data()
+        print("Skip grams: {}".format(X_y))
+        self.assertEqual(vocabulary_size, 11)
 
 
 if __name__ == '__main__':
