28b73fc41d6cc2f39dea6a875b6a61a4059368c6
David Ström <david.strom@callistaenterprise.se>
2021-11-11 16:55:11 +0100

    WIP

--- word2vec/tests/training_data_builder_test.py
+++ word2vec/tests/training_data_builder_test.py
@@ -1,6 +1,7 @@
 import unittest
 from app.preprocessing.training_data_builder import TrainingDataBuilder
 from os import path
+import logging
 
 
 class TrainingDataBuilderTest(unittest.TestCase):
@@ -9,14 +10,14 @@ class TrainingDataBuilderTest(unittest.TestCase):
         dir_name = path.dirname(__file__)
         self.training_data_builder = TrainingDataBuilder(cleaned_file=path.join(dir_name, 'test_cleaned_train_data.txt'), window_size=2, dry_run=True)
         vocabulary_size, X_y = self.training_data_builder.build_cbow_training_data()
-        print("CBOW training samples: {}".format(X_y))
+        logging.info("CBOW training samples: {}", X_y)
         self.assertEqual(vocabulary_size, 11)
 
     def test_generate_sg_training_samples(self):
         dir_name = path.dirname(__file__)
         self.training_data_builder = TrainingDataBuilder(cleaned_file=path.join(dir_name, 'test_cleaned_train_data.txt'), window_size=2, dry_run=True)
         vocabulary_size, X_y = self.training_data_builder.build_sg_training_data()
-        print("Skip grams: {}".format(X_y))
+        logging.info("Skip grams: {}", X_y)
         self.assertEqual(vocabulary_size, 11)
 
     def test_generate_glove_training_samples(self):
