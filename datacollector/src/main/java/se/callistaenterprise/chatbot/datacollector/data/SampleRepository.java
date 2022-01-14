d3dacfbcbacd78fc9f7ef0138dea65b2f105720c
David Ström <david.strom@callistaenterprise.se>
2022-01-14 21:34:51 +0100

    deleting old test projects

--- datacollector/src/main/java/se/callistaenterprise/chatbot/datacollector/data/SampleRepository.java
+++
@@ -1,6 +0,0 @@
-package se.callistaenterprise.chatbot.datacollector.data;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface SampleRepository extends ReactiveCrudRepository<SampleEntity, Long> {
-}
